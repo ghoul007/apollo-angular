852bf8846207715d615edd0006204aefc18f89e7
Ahmed <ghoul.ahmed5@gmail.com>
2018-01-30 23:51:21 +0100

    add app module config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
+import { ApolloModule, Apollo } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { url } from "inspector";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, ApolloModule, HttpLinkModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(apollo: Apollo, httplink: HttpLink) {
+    apollo.create({
+      link: httplink.create({
+        url: "",
+        cache:new InMemoryCache()
+      })
+    });
+  }
+}
