25f58449ac9b1e614038323c2961fc14cd531c15
Ahmed <ghoul.ahmed5@gmail.com>
2018-02-05 21:25:00 +0100

    watchQuey exampleé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,19 @@ import { ApolloModule, Apollo } from "apollo-angular";
 import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
 
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { url } from "inspector";
+import { ListComponent } from "./list/list.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListComponent],
   imports: [BrowserModule, HttpClientModule, ApolloModule, HttpLinkModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(apollo: Apollo, httplink: HttpLink) {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
     apollo.create({
-      link: httplink.create({
-        url: "",
-        cache:new InMemoryCache()
-      })
+      link: httpLink.create({ uri: "https://vm8mjvrnv3.lp.gql.zone/graphql" }),
+      cache: new InMemoryCache()
     });
   }
 }
